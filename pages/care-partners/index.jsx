cea7ef95bb2c16a8e9228baa0b8f3470d88a1778
Amr Hassan <amr.hassan.emam@gmail.com>
2024-08-17 23:13:20 +0300

    temporarily partners pages

--- pages/care-partners/index.jsx
+++ pages/care-partners/index.jsx
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Image from 'next/image';
 
-const CarePartners = () => {
-	return <div>CarePartners</div>;
+import { client } from '../../src/libs/contentful';
+import { useContentContext } from '../../src/context/ContentContext';
+import HeroSection from '../../src/components/care/HeroSection';
+import IdeaSection from '../../src/components/care/IdeaSection';
+import GoalsSection from '../../src/components/care/GoalsSection';
+import Partners from '../../src/components/care/Partners';
+import Head from 'next/head';
+const CarePartners = ({ sahsehCareData }) => {
+	const { setNavbar, mediaFiles, setMediaFiles, setFooter } =
+		useContentContext();
+
+	useEffect(() => {
+		setFooter(sahsehCareData.footer);
+		setNavbar(sahsehCareData.navbar);
+		setMediaFiles({
+			...mediaFiles,
+			logo: sahsehCareData.logo,
+			whiteLogo: sahsehCareData.whiteLogo,
+		});
+	}, [sahsehCareData]);
+
+	return (
+		<>
+			<Head>
+				<title>{sahsehCareData?.sahsehCareContent.seoTitle}</title>
+				<meta
+					name="description"
+					content={sahsehCareData?.sahsehCareContent.seoDescription}
+				/>
+				<meta
+					name="keywords"
+					content={
+						sahsehCareData?.sahsehCareContent.seoKeywords &&
+						sahsehCareData?.sahsehCareContent.seoKeywords
+					}
+				/>
+			</Head>
+
+			<div className="mb-12 -mt-12 lg:-mt-24 lg:mb-24">
+				<Partners
+					sahsehCareContent={sahsehCareData.sahsehCareContent}
+					mediaFiles={mediaFiles}
+				/>
+			</div>
+		</>
+	);
 };
 
 export default CarePartners;
+
+export async function getStaticProps({ locale }) {
+	const navbar = await client.getEntry('4Gg2tT8MmLwkIQyYyhTckP', {
+		locale,
+	});
+	const footer = await client.getEntry('7Hmn6qQE2OZ6X4mkzoSfwe', {
+		locale,
+	});
+	const sahsehCareContent = await client.getEntry('NeVSDOOvG5rZTk3QXvlVZ', {
+		locale,
+	});
+	const logo = await client.getAsset('2pn2arIDDrmXgxdHY0O7Tz', {
+		locale,
+	});
+	const whiteLogo = await client.getAsset('1hk88EcrYjfrohZJuLEie2', {
+		locale,
+	});
+
+	return {
+		props: {
+			sahsehCareData: {
+				sahsehCareContent: sahsehCareContent.fields,
+				navbar: navbar.fields,
+				whiteLogo: whiteLogo.fields.file,
+				footer: footer.fields,
+				logo: logo.fields.file,
+			},
+		},
+	};
+}
--- pages/nutrition-partners/index.jsx
+++ pages/nutrition-partners/index.jsx
@@ -1,7 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Image from 'next/image';
 
-const NutritionPartners = () => {
-	return <div>NutritionPartners</div>;
+import { client } from '../../src/libs/contentful';
+import { useContentContext } from '../../src/context/ContentContext';
+import HeroSection from '../../src/components/nutrition/HeroSection';
+import IdeaSection from '../../src/components/nutrition/IdeaSection';
+import GoalsSection from '../../src/components/nutrition/GoalsSection';
+import Partners from '../../src/components/nutrition/Partners';
+import Head from 'next/head';
+const NutritionPartners = ({ sahsehNutritionData }) => {
+	const { setNavbar, mediaFiles, setMediaFiles, setFooter } =
+		useContentContext();
+
+	useEffect(() => {
+		setFooter(sahsehNutritionData.footer);
+		setNavbar(sahsehNutritionData.navbar);
+		setMediaFiles({
+			...mediaFiles,
+			logo: sahsehNutritionData.logo,
+			whiteLogo: sahsehNutritionData.whiteLogo,
+		});
+	}, [sahsehNutritionData]);
+
+	return (
+		<>
+			<Head>
+				<title>{sahsehNutritionData?.nutritionPageContent.seoTitle}</title>
+				<meta
+					name="description"
+					content={sahsehNutritionData?.nutritionPageContent.seoDescription}
+				/>
+				<meta
+					name="keywords"
+					content={
+						sahsehNutritionData?.nutritionPageContent.seoKeywords &&
+						sahsehNutritionData?.nutritionPageContent.seoKeywords
+					}
+				/>
+			</Head>
+			<div className="mb-24 lg:mb-44">
+				<Partners
+					mediaFiles={mediaFiles}
+					nutritionContent={sahsehNutritionData.nutritionPageContent}
+				/>
+			</div>
+		</>
+	);
 };
 
 export default NutritionPartners;
+
+export async function getStaticProps({ locale }) {
+	const navbar = await client.getEntry('4Gg2tT8MmLwkIQyYyhTckP', {
+		locale,
+	});
+	const footer = await client.getEntry('7Hmn6qQE2OZ6X4mkzoSfwe', {
+		locale,
+	});
+	const nutritionPageContent = await client.getEntry('2dzKefw7fRK9eBU7VN3nib', {
+		locale,
+	});
+	const logo = await client.getAsset('2pn2arIDDrmXgxdHY0O7Tz', {
+		locale,
+	});
+	const whiteLogo = await client.getAsset('1hk88EcrYjfrohZJuLEie2', {
+		locale,
+	});
+
+	return {
+		props: {
+			sahsehNutritionData: {
+				nutritionPageContent: nutritionPageContent.fields,
+				whiteLogo: whiteLogo.fields.file,
+				navbar: navbar.fields,
+				footer: footer.fields,
+				logo: logo.fields.file,
+			},
+		},
+	};
+}
